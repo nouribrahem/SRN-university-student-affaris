bf032a55a0206370b71282ef7ab7e809f78f4675
NourhanE.Hassan <92875566+nouribrahem@users.noreply.github.com>
2023-04-14 13:44:15 +0200

    home nav edits

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const nav = document.querySelector('.Navigation')
-fetch('/Navigation.html').then(res=>res.text()).then(data=>{
-    nav.innerHtml = data
-})
+// const nav = document.querySelector('.Navigation')
+// fetch('/Navigation.html').then(res=>res.text()).then(data=>{
+//     nav.innerHtml = data
+// })
