3af322867b8607942ac06811cada0eacbf9bf72a
NourhanE.Hassan <92875566+nouribrahem@users.noreply.github.com>
2023-04-16 22:41:06 +0200

    edits in search

--- viewScript.js
+++ viewScript.js
@@ -26,12 +26,14 @@ function showData(){
         var cell_3 = row.insertCell();
         var cell_4 = row.insertCell();
         var cell_5 = row.insertCell();
+        var cell_6 = row.insertCell();
 
         cell_1.innerHTML = arr[i].name;
         cell_2.innerHTML = arr[i].ID;
         cell_3.innerHTML = arr[i].status;
         cell_4.innerHTML = arr[i].level;
         cell_5.innerHTML = arr[i].department;
+        cell_6.innerHTML = '<div id="actions"><button id="status">Status</button><button id="assign"><a href="UpdateStudent.html" style="color:white;">Update<a/></button><button id="update"><a href="AssignDepartment.html" style="color:white;">Assign Department<a/></button></div>';
 
     }
 }
