83ab508a321eba541847d002a241a139ba2a8cda
NourhanE.Hassan <92875566+nouribrahem@users.noreply.github.com>
2023-04-17 16:14:26 +0200

    search change activity done

--- searchScript.js
+++ searchScript.js
@@ -1,3 +1,12 @@
+var arr=[];
+
+function getData(){
+    
+    let data = window.localStorage.getItem("students");
+    if(data != null){
+        arr =JSON.parse(data);
+    }
+}
 function getActive(){
     
     let data = window.localStorage.getItem("students");
@@ -8,6 +17,7 @@ function getActive(){
     arr = arr.filter((element)=>{return element.status=='Active'});
     return arr;
 }
+
 function showSearch(arr){
     // console.log(arr);
     var tbl = document.getElementById("students");
@@ -35,7 +45,7 @@ function showSearch(arr){
             cell_3.innerHTML = arr[i].status;
             cell_4.innerHTML = arr[i].level;
             cell_5.innerHTML = arr[i].dep;
-            cell_6.innerHTML = '<div id="actions"><button id="status">Status</button><button id="assign">'+
+            cell_6.innerHTML = '<div id="actions"><button id="status" onClick = "changeStatus(this)">Status</button><button id="assign">'+
             '<a href="' + linkUpdate+'" style="color:white;">Update<a/></button><button id="update">'+
             '<a href="'+linkAssignDep +'"  style="color:white;">Assign Department<a/></button></div>';
     
@@ -81,6 +91,31 @@ function search(){
     }
     showSearch(found);
     
+}
+function updateStatusInStorage(id, status) {
+    getData();
+
+    for (let i = 0; i < arr.length; i++) {
+        if (id == arr[i].ID) {
+            arr[i].status = status;
+        }
+    }
+    window.localStorage.setItem('students', JSON.stringify(arr));
+
+}
+function changeStatus(button) {
+    var row = button.parentNode.parentNode.parentNode; // get the row element
+
+    var idCell = row.cells[1];
+    var statusCell = row.cells[2]; // get the cell containing the status
+    var status = statusCell.innerHTML; // get the current status
+
+    if (status === "Active") {
+        statusCell.innerHTML = "Inactive"; // change the status
+    } else {
+        statusCell.innerHTML = "Active"; // change the status back
+    }
+    updateStatusInStorage(idCell.innerHTML, statusCell.innerHTML);
 }
 // search();
 document.getElementById("submit").addEventListener("click", search);
