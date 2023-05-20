933ff79bb074f851905e37cfb7a29c55206d4ce3
NourhanE.Hassan <92875566+nouribrahem@users.noreply.github.com>
2023-05-20 11:38:15 +0300

    django edits

--- studentAffairs/students/models.py
+++ studentAffairs/students/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
+class students(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
 # Create your models here.
--- studentAffairs/students/views.py
+++ studentAffairs/students/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import students
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the students index.")
+# Create your views here.
+
+# def index(request):
+#     template = loader.get_template('haha.html')
+#     return HttpResponse(template.render())
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the students index.")
-# Create your views here.
+    student_list = students.objects.all(). values()
+    output = ""
+    for x in student_list:
+        output += x["firstname"]
+        output+=" "
+    return HttpResponse(output)  
