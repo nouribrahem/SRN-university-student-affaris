31401f1d512d15d96951e0281210085feb5887c4
NourhanE.Hassan <92875566+nouribrahem@users.noreply.github.com>
2023-05-24 23:06:56 +0300

    انل مبقتش حمل الويب و الجانجو

--- studentAffairs/students/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-class Students(models.Model):
-    fname = models.CharField(max_length=255)
-    lname = models.CharField(max_length=255)
-    id = models.CharField(max_length=255,primary_key=True) 
-    status = models.CharField(max_length=255)
-    level = models.IntegerField()
-    department = models.CharField(max_length=20)
-    gpa = models.DecimalField(max_digits=3,decimal_places=2)
