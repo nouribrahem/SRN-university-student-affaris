2546ffe6c5f408b2c9ba4320fca2c2fcb4be7513
rawwanntarekk <11410120211039@stud.cu.edu.eg>
2023-05-25 00:22:28 +0300

    yalaaaa

--- student_affairs/our_students/urls.py
+++ student_affairs/our_students/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views 
 urlpatterns = [
     path('', views.index, name='index'),
+    # عايزين نجرب بعد كدا لو هينفع نغير اول اسم ارجيومينت
+    path('add_studentfun/', views.add_studentfun, name='add_studentfun')
 ]
