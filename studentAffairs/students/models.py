1ee91b80f26030964a128440add5450c9e9d4470
NourhanE.Hassan <92875566+nouribrahem@users.noreply.github.com>
2023-05-24 18:05:09 +0300

    added students table

--- studentAffairs/students/models.py
+++ studentAffairs/students/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-class students(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
+class Students(models.Model):
+    fname = models.CharField(max_length=255)
+    lname = models.CharField(max_length=255)
+    id = models.CharField(max_length=255,primary_key=True) 
+    status = models.CharField(max_length=255)
+    level = models.IntegerField()
+    department = models.CharField(max_length=20)
+    gpa = models.DecimalField(max_digits=3,decimal_places=2)
 # Create your models here.
