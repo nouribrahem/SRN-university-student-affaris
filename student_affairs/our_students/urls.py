d147d6f20e5e3a000874fb90dda61583810f1296
Rawan Younis <41347412+Rawanyounis123@users.noreply.github.com>
2023-05-25 15:25:18 +0300

    Add Assign Dep

--- student_affairs/our_students/urls.py
+++ student_affairs/our_students/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('assign_dep/<str:id>',views.assign_dep),
+    path('update-department/<str:id>/',views.update_dep),
 ]
--- student_affairs/our_students/views.py
+++ student_affairs/our_students/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import students
+from django.http import JsonResponse
+import json
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the students index.")
 # Create your views here.
@@ -17,3 +19,29 @@ def index(request):
         output += x["fname"]
         output+=" "
     return HttpResponse(output)
+
+def assign_dep(request,id):
+    student_info = students.objects.get(id=id)
+    content = {
+        'id': student_info.id,
+        'name': student_info.fname + ' ' + student_info.lname,
+        'level':student_info.level,
+        'gpa':student_info.gpa,
+        'department':student_info.department,
+    }
+    department_options = ["Computer Science", "Information System", "Information Technology", "Decision Support",
+                          "Artificial Intelligence", "Software Engineering", "Bioinformatics", "Data Science", "General"]
+    dep_page_info = render(request, 'AssignDepartment.html', context={'content': content, 'department_options': department_options})
+    return HttpResponse(dep_page_info)
+
+def update_dep(request,id):
+    if request.method == 'POST':
+        data = json.loads(request.body) 
+        print(data)
+        dep = data.get('department')
+        print(dep)
+        stud = students.objects.get(id=id)    
+        stud.department = dep
+        stud.save()
+        return JsonResponse({'message': 'Update successful'})
+    return JsonResponse({'message': 'Invalid request'})
